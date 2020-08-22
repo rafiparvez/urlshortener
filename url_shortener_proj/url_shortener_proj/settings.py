c8a86f78a91dbbac453c69fce25a040284759c0b
rafiparvez <rafiparvez@gmail.com>
2020-08-21 22:07:00 -0400

    added postgres as backend datastore

--- url_shortener_proj/url_shortener_proj/settings.py
+++ url_shortener_proj/url_shortener_proj/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'url_shortener_proj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'url_shortener_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
